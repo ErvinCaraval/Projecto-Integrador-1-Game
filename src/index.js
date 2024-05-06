5beffa16a82bb42a4dda891568dc7ffb4f18ed88
Estiven <estivenfrv@gmail.com>
2024-05-05 22:18:59 -0500

    New Version

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
+import { StrictMode } from "react";
+import Experience from "./Experience";
 import "./styles.css";
 import { createRoot } from "react-dom/client";
-import Experience from "./Experience";
-import { Canvas } from "@react-three/fiber";
 
 const root = createRoot(document.getElementById("root"));
-
-
-
-root.render(<>
-    <Canvas
-
-        camera={{position:[-50,0,0], rotation:[0,0,0]}}
-        shadows={true}
-    >
+root.render(
+    <StrictMode>
         <Experience />
-    </Canvas>
-    {/* <Loader /> */}
-</>
-)
+    </StrictMode>
+);
